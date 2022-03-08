04ff71f76f59df79b2b285a025ec91b0794d83e9
Yury Getman <stagg7777@gmail.com>
2022-03-08 21:57:55 +0300

    implements apps and icons GET methods

--- api/v1-router.js
+++ api/v1-router.js
@@ -1,4 +1,8 @@
+import fs from "fs";
+
 import express from "express";
+
+import { appManager, iconManager } from "../core/apps.js";
 
 
 export default function () {
@@ -6,6 +10,29 @@ export default function () {
 
     router.get("/", (_, res) => {
         res.json({ res: "Success" });
+    });
+
+    router.get("/apps", (_, res) => {
+        res.json(appManager.getSupportedApps());
+    });
+
+    router.get("/apps/:appName", (req, res) => {
+        res.send("Search app" + req.params.appName);
+    });
+
+    router.get("/apps/:appName/icon", async (req, res) => {
+        const appName = req.params.appName;
+        const iconPath = "./core/apps/icons/" + iconManager.getIconPathByAppName(appName);
+        try {
+            await fs.promises.access(iconPath, fs.constants.R_OK);
+            res.sendFile(iconPath, { root: "./" });
+        } catch (err) {
+            res.json({
+                res: "Error",
+                description: "Cannot send app icon " + appName,
+                reason: JSON.stringify(err)
+            });
+        }
     });
 
     return router;
