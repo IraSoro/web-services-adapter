cc5c65b10253ec341b8f4e1113916c4eea1fc179
Yury Getman <stagg7777@gmail.com>
2022-03-11 15:07:14 +0300

    implements connection checking in the AppInfo

--- ui/pages/app-info.jsx
+++ ui/pages/app-info.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, {
+    useState,
+    useEffect
+} from "react";
 import { useParams } from "react-router-dom";
 import {
     Avatar,
@@ -10,6 +13,20 @@ import { PageWrapper } from "../components";
 
 
 export const AppInfo = () => {
+    const [connected, setConnected] = useState(false);
+
+    useEffect(() => {
+        fetch(`/api/v1/apps/${appName}`)
+            .then((resp) => resp.json())
+            .then((apps) => {
+                if (!apps.length) {
+                    return;
+                }
+                setConnected(apps[0].connected);
+            })
+            .catch((err) => console.error(err));
+    }, []);
+
     const { appName } = useParams();
 
     const style = {
@@ -18,13 +35,16 @@ export const AppInfo = () => {
         alignItems: "center"
     };
 
+    const actionButton = connected ? <Button variant="contained">Create</Button>
+        : <Button variant="outlined">Connect</Button>;
+
     return (
         <PageWrapper>
             <Box sx={style}>
                 <Avatar variant="square" src={"/api/v1/apps/" + appName + "/icon"} />
             </Box>
             <Box sx={style}>
-                <Button variant="outlined">Connect</Button>
+                {actionButton}
             </Box>
         </PageWrapper>
     );
